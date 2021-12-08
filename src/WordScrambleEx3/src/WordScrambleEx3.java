65f9f4e6479aac2dc64f276cea338c7518f818ad
김유빈 <ybkim@keona.co.kr>
2021-12-08 09:26:09 +0900

    feat: WordScrambleEx3 추가

---
+++ src/WordScrambleEx3/src/WordScrambleEx3.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class WordScrambleEx3 {
+    public static void main(String[] args) {
+        String[] strArr = { "CHANGE", "LOVE", "HOPE", "VIEW"};
+
+        String answer, question, userAnswer;
+        Scanner scanner = new Scanner(System.in);
+
+        newGameLoop: while (true) {
+            answer = getAnswer(strArr);
+            question = getScrambledWord(answer);
+
+            while (true) {
+                System.out.println("Question :" + question);
+                System.out.print("Your answer is :");
+
+                userAnswer = scanner.nextLine();
+
+                if (userAnswer.toUpperCase().equals("Q")) {
+                    System.out.println("게임을 종료하였습니다.");
+                    break newGameLoop;
+                }
+
+                if (userAnswer.toUpperCase().equals(answer)) {
+                    System.out.println("정답입니다.");
+                    break;
+                } else {
+                    System.out.println(userAnswer + "은/는 정답이 아닙니다. 다시 시도해보세요.");
+                }
+            }
+
+        }
+
+    }
+
+    private static String getAnswer(String[] strArr) {
+        int idx = (int)(Math.random()*strArr.length);
+        return strArr[idx];
+    }
+
+    private static String getScrambledWord(String str) {
+        char[] chArr = str.toCharArray();
+
+        for(int i=0;i < str.length();i++) {
+
+
+            int idx = (int)(Math.random()*str.length());
+
+            char tmp = chArr[i];
+            chArr[i] = chArr[idx];
+            chArr[idx] = tmp;
+        }
+
+        return new String(chArr);
+    }
+}
